b4a7ca6e52d9707bec3c62f3ea6f14a85f757e3c
nclong92 <nclong92@gmail.com>
2019-12-21 17:40:49 +0700

    employee service

---
+++ Paycompute.Services/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using Paycompute.Entity;
+using Paycompute.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paycompute.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(Employee newEmployee)
+        {
+            await _context.Employees.AddAsync(newEmployee);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<Employee> GetAll() =>
+            _context.Employees;
+
+        public Employee GetById(int employeeId) => _context.Employees.Where(e => e.Id == employeeId).FirstOrDefault();
+
+        public async Task UpdateAsync(Employee employee)
+        {
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id)
+        {
+            var employee = GetById(id);
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal UnionFees(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
